8d272f0013457344c796520e8a5929dfb0eed756
bbence93 <c2bodben.c2@gmail.com>
2026-03-26 17:29:00 +0100

    playwright.config.ts módosítása: screenshot hozzáadása hiba esetén, slowMo hozzáadása és kikommentelése könnyebb beállítás miatt

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,13 +24,20 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  
   use: {
+  
+  //   launchOptions:{
+  //     slowMo: 500
+  //   },
+
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     testIdAttribute: 'data-test',
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure'//,
     // headless: false,
   },
 
